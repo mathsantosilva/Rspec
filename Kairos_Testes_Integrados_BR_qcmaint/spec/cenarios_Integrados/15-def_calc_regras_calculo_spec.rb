e7439f6b2dbd02f0120c27921b11df58bde9a627
Emerson Macedo <emerson.macedo1@hotmail.com>
2021-08-20 14:20:38 -0300

    Alteração de acesso ao kairos - aceite dos cookies - alteração de acesso a criação de Regra de Cálculo - fechar janela de ajuda

--- Kairos_Testes_Integrados_BR_qcmaint/spec/cenarios_Integrados/15-def_calc_regras_calculo_spec.rb
+++ Kairos_Testes_Integrados_BR_qcmaint/spec/cenarios_Integrados/15-def_calc_regras_calculo_spec.rb
@@ -1,16 +1,30 @@
 describe 'estrutura', :regras_calculo do
   before(:each) do
-    visit '/Dimep/Account/LogOn?ReturnUrl=%2F'
+    visit '/'
 
-    fill_in 'LogOnModel_UserName', with: 'migracao15042021@dimep.com'
-    fill_in 'LogOnModel_Password', with: '1'
+    #Aceitando os cookies
+    find('div[class="cc-compliance"]', text: 'Aceitar e fechar').click
+    sleep 2 
+
+    fill_in 'LogOnModel_UserName', with: @login
+    fill_in 'LogOnModel_Password', with: @senha
     click_button 'Entrar'
 
-    visit '/admin/RegrasCalculo'
-    visit '/admin/RegrasCalculo/Create'
+    # Acessando o cadastro de Horário pela navegação de telas
+    selecionar = find('div[id="toggleUserDefinitions"]')
+    selecionar.hover
+    sleep 2
+    find('div[class="pointer DropDownHeaderElement"]', text: 'Cálculo').click
+
+    #Fechar janela de ajuda
+    sleep 2
+    find('div[aria-label= "Fechar diálogo"]').click
+
+    find('div[id="MenuRegrasCalculo"]').click
   end
 
   it 'CT015 Inserir novas regras de calculo', :ct015_regras_calculo do
+    find('label[class="pointer"]', text: 'Nova Regra').click
     fill_in 'Codigo', with: '127763'
     fill_in 'Descricao', with: 'azffsc'
 
