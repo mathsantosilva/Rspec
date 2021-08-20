0bfcdfd275dc51248356c457f22b5d6f2acb4e60
Matheus Santos <matheus.santos.x4@gmail.com>
2021-08-20 08:16:33 -0300

    Retirado os sleep para uma execução mais rapida

--- Kairos_Testes_Integrados_BR_qcmaint/spec/cenarios_Integrados/0-princ_login_spec.rb
+++ Kairos_Testes_Integrados_BR_qcmaint/spec/cenarios_Integrados/0-princ_login_spec.rb
@@ -5,7 +5,6 @@ describe 'login', :login do
 
     #Aceitando os cookies
     find('div[class="cc-compliance"]', text: 'Aceitar e fechar').click
-    sleep 2
 
     fill_in 'LogOnModel_UserName', with: @login
     fill_in 'LogOnModel_Password', with: @senha
@@ -14,14 +13,10 @@ describe 'login', :login do
     find('img[class="DMPLogoHeader"]').click
     nome_empresa = find('div[class="TabMenuNomeEmpresa shortText"]')
     expect(nome_empresa).to have_content 'Empresa Br - Automação de testes - 17-08-2021 - 1101'
-    sleep 2
 
     #Clicando no botão de deslogar
     selecionar = find('span[id="toogleUserName"]')
     selecionar.hover
-    sleep 2
     find('span[style="line-height:20px;margin-left:4px;"]', text: 'Sair da aplicação').click
-    sleep 2
-
   end
 end
