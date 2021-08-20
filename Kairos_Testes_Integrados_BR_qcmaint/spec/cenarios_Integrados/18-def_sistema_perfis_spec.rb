2fd91d14cd4a0f9a5882d87b302a1c771f1bf489
Emerson Macedo <emerson.macedo1@hotmail.com>
2021-08-20 17:50:04 -0300

    Alteração de acesso ao kairos - aceite dos cookies - alteração de acesso a criação de Perfil

--- Kairos_Testes_Integrados_BR_qcmaint/spec/cenarios_Integrados/18-def_sistema_perfis_spec.rb
+++ Kairos_Testes_Integrados_BR_qcmaint/spec/cenarios_Integrados/18-def_sistema_perfis_spec.rb
@@ -1,16 +1,27 @@
 describe 'perfis do sistema', :perfil do
   before(:each) do
-    visit '/Dimep/Account/LogOn?ReturnUrl=%2F'
+    visit '/'
 
-    fill_in 'LogOnModel_UserName', with: 'migracao15042021@dimep.com'
-    fill_in 'LogOnModel_Password', with: '1'
+    #Aceitando os cookies
+    find('div[class="cc-compliance"]', text: 'Aceitar e fechar').click
+    sleep 2 
+
+    fill_in 'LogOnModel_UserName', with: @login
+    fill_in 'LogOnModel_Password', with: @senha
     click_button 'Entrar'
 
-    visit '/admin/Usuarios'
-    visit '/admin/Perfis/Create'
+    # Acessando o cadastro de Perfil pela navegação de telas
+    selecionar = find('div[id="toggleUserDefinitions"]')
+    selecionar.hover
+    sleep 2
+
+    find('div[class="pointer DropDownHeaderElement"]', text: 'Sistema').click
+    find('div[id="MenuPerfis"]').click
   end
 
   it 'CT018 Inserir um novo perfil', :ct018_perfil do
+
+    find('label[class="pointer"]', text: 'Novo Perfil').click    
     fill_in 'Perfil_Descricao', with: 'teste'
     drop = find("input[type=button][value='Salvar']", match: :first)
     drop.click
