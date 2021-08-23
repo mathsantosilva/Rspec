28cfc7b2202fac1695733a46546d24bc8a6e553c
Emerson Macedo <emerson.macedo1@hotmail.com>
2021-08-23 11:16:26 -0300

    Alteração de acesso ao kairos - aceite dos cookies - Inclusão de campos extra para cargo

--- Kairos_Testes_Integrados_BR_qcmaint/spec/cenarios_Integrados/19-def_sistema_campos_extra_spec.rb
+++ Kairos_Testes_Integrados_BR_qcmaint/spec/cenarios_Integrados/19-def_sistema_campos_extra_spec.rb
@@ -1,23 +1,47 @@
-describe 'guada a estrutura organizacional', :campos do
+describe 'Cadastrar campos extra de estrutura organizacional e cargo', :campos do
   before(:each) do
-    visit '/Dimep/Account/LogOn?ReturnUrl=%2F'
+    visit '/'
 
-    fill_in 'LogOnModel_UserName', with: 'migracao15042021@dimep.com'
-    fill_in 'LogOnModel_Password', with: '1'
+    #Aceitando os cookies
+    find('div[class="cc-compliance"]', text: 'Aceitar e fechar').click
+    sleep 2 
+
+    fill_in 'LogOnModel_UserName', with: @login
+    fill_in 'LogOnModel_Password', with: @senha
     click_button 'Entrar'
     
-    visit '/admin/Usuarios'
-    visit '/admin/CamposExtra'
+    # Acessando o cadastro de Campos extra pela navegação de telas
+    selecionar = find('div[id="toggleUserDefinitions"]')
+    selecionar.hover
+    sleep 2
+
+    find('div[class="pointer DropDownHeaderElement"]', text: 'Sistema').click
+    find('div[id="MenuCamposExtra"]').click
+    
   end
 
-  it 'CT019 Inserir novos campos extras', :ct019_campos do
+  it 'CT019 Inserir novos campos extras de estruturas', :ct019_campos do
+    
+    #Adicionando campos extra de estrutura
+    find('label[for="Campo1"]', visible: false).set(true)
     fill_in 'LabelCampo1', with: '11'
-    fill_in 'LabelCampo2', with: '11'
+    find('label[for="Campo2"]', visible: false).set(true)
+    fill_in 'LabelCampo2', with: 'aa'
 
     drop = find("input[type=button][value='Salvar']", match: :first)
     drop.click
-    # não é necessario selecionar item.
-
     expect(find('div[id=Summary-Field-Index]')).to have_content 'Campos extra da estrutura organizacional guardados com sucesso.'
+
+    #Adicionando campos extra de cargo
+    find('span[id="TabCargo"]', text: 'Cargo').click
+    find('label[for="CargoCampo1"]', visible: false ).set(true)
+    fill_in 'CargoLabelCampo1', with: '22'
+    find('label[for="CargoCampo2"]', visible: false).set(true)
+    fill_in 'CargoLabelCampo2', with: 'bb'    
+
+    drop = find("input[type=button][value='Salvar']", match: :first)
+    drop.click
+    expect(find('div[id=Summary-Field-Index]')).to have_content 'Campos extra do cargo guardados com sucesso.'
+  
   end
 end
