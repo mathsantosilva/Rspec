451e6d86792397ca0c3bd57168532f2d354f7258
Emerson Macedo <emerson.macedo1@hotmail.com>
2021-08-23 11:41:26 -0300

    Alteração de acesso ao kairos - aceite dos cookies - alteração de acesso a criação de formato de importação de estrutura

--- Kairos_Testes_Integrados_BR_qcmaint/spec/cenarios_Integrados/20-def_importacao_formatos_arquivo_estrutura_spec.rb
+++ Kairos_Testes_Integrados_BR_qcmaint/spec/cenarios_Integrados/20-def_importacao_formatos_arquivo_estrutura_spec.rb
@@ -1,23 +1,33 @@
 describe 'importacao estrutura', :importacao_estrutura do
   before(:each) do
-    visit '/Dimep/Account/LogOn?ReturnUrl=%2F'
+    visit '/'
 
-    fill_in 'LogOnModel_UserName', with: 'migracao15042021@dimep.com'
-    fill_in 'LogOnModel_Password', with: '1'
+    #Aceitando os cookies
+    find('div[class="cc-compliance"]', text: 'Aceitar e fechar').click
+
+    #Informações de login
+    fill_in 'LogOnModel_UserName', with: @login
+    fill_in 'LogOnModel_Password', with: @senha
     click_button 'Entrar'
 
-    visit '/admin/FormatosArquivoColetaApontamentos'
-    visit '/admin/FormatosArquivoColetaApontamentos/EditEstrutura'
+    # Acessando o cadastro de Formato de estrutura pela navegação de telas
+    selecionar = find('div[id="toggleUserDefinitions"]')
+    selecionar.hover
+    sleep 2
+    find('div[class="pointer DropDownHeaderElement"]', text: 'Importação').click
+
   end
 
   it 'CT020 Inserir campos na importação estrutura', :ct020_importacao_estrutura do
-    fill_in 'FormatoArquivoColetaApontamentos_Codigo', with: '12233'
+    
+    #Adicionar cadastro de formato 
+    find('label[class="pointer"]' , text:'Adicionar' ).click
+    fill_in 'FormatoArquivoColetaApontamentos_Codigo', with: '123'
     fill_in 'FormatoArquivoColetaApontamentos_Descricao', with: 'abcdz'
     fill_in 'tbInicio2011', with: '1233'
     fill_in 'tbFim2011', with: '12323'
     fill_in 'tbInicio2012', with: '7547471'
     fill_in 'tbFim2012', with: '42357479'
-
     drop = find("input[type=button][value='Salvar']", match: :first)
     drop.click
 
