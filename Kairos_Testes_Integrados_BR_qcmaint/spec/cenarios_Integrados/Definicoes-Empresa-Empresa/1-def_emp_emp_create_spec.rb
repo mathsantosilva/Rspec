f247328b161e70651e4871de4cdb3d03476ccd5e
Matheus Santos <matheus.santos.x4@gmail.com>
2021-09-08 14:27:38 -0300

    Atualizado o webdrive e dividido o projeto em pastas

---
+++ Kairos_Testes_Integrados_BR_qcmaint/spec/cenarios_Integrados/Definicoes-Empresa-Empresa/1-def_emp_emp_create_spec.rb
@@ -0,0 +1,51 @@
+describe 'empresa', :empresa_create do
+  before(:each) do
+    visit '/'
+    expect(page.title).to eql 'kairos'
+
+    fill_in 'LogOnModel_UserName', with: @login
+    fill_in 'LogOnModel_Password', with: @senha
+    click_button 'Entrar'
+    # Acessando a empresa pela navegação de telas
+    selecionar = find('div[id="toggleUserDefinitions"]')
+    selecionar.hover
+    sleep 3
+    find('div[class="pointer DropDownHeaderElement"]', text: 'Empresa').click
+    find('div[id="MenuEmpresas"]').click
+    find('label[class="pointer"]').click
+    
+
+    # Acessar a empresa direto pela URL
+    # visit '/admin/Empresas'
+    # visit '/admin/Empresas/Create'
+  end
+
+  it 'CT001 Inserir uma nova empresa' do
+    fill_in 'Empresa_Codigo', with: '21438506'
+    fill_in 'Empresa_CEI', with: '233257875388'
+    fill_in 'Empresa_Telefone', with: '999999999'
+    find('label[for="CbUtilizaPortaria"]', visible: true)
+    fill_in 'Empresa_RazaoSocial', with: times
+    fill_in 'Empresa_CnpjCpf', with: @cnpj_cpf
+    select('INDÚSTRIAS EXTRATIVAS', from: 'Empresa_RamoAtividade_Id').select_option
+    fill_in 'Empresa_Endereco', with: 'rua abcd'
+    fill_in 'Empresa_Bairro', with: 'abcd'
+    fill_in 'Empresa_Cidade', with: 'abcd'
+    fill_in 'Empresa_UF', with: 'SP'
+    fill_in 'Empresa_Pais', with: 'Brasil'
+    fill_in 'Empresa_URL', with: 'https://www.dimepkairos.com.br/Dimep/Account/LogOn?ReturnUrl=%2F'
+    fill_in 'Empresa_Email', with: 'kairos@kairos.com'
+    fill_in 'Empresa_DataAbertura', with: '10112020'
+    select('UTC - 12', from: 'Empresa_FusoHorario_Id').select_option
+    find('label[for="checkHabilitaHorarioVerao"]', visible: false).set(true)
+    fill_in 'Empresa_InicioHorarioVerao', with: '10032021'
+    fill_in 'Empresa_FimHorarioVerao', with: '10052021'
+    fill_in 'Empresa_DataPrevisaoProximoFechamento', with: '10052021'
+    fill_in 'Empresa_DataLimiteTratamentoPonto', with: '10072021'
+    fill_in 'connectChave_ChaveConnect', with: 'S123ab&*(#$'
+    drop = find("input[type=button][value='Salvar']", match: :first)
+    drop.click
+
+    expect(find('div[id=Summary-Field-Index]')).to have_content 'A Empresa foi criada com sucesso'
+   end  
+  end
---
+++ Kairos_Testes_Integrados_BR_qcmaint/spec/cenarios_Integrados/Definicoes-Empresa-Empresa/2-def_emp_emp_update_spec.rb
@@ -0,0 +1,56 @@
+describe 'empresa', :empresa_update do
+  before(:each) do
+    visit '/'
+    expect(page.title).to eql 'kairos'
+
+    fill_in 'LogOnModel_UserName', with: @login
+    fill_in 'LogOnModel_Password', with: @senha
+    click_button 'Entrar'
+    # Acessando a empresa pela navegação de telas
+    selecionar = find('div[id="toggleUserDefinitions"]')
+    selecionar.hover
+    sleep 3
+    find('div[class="pointer DropDownHeaderElement"]', text: 'Empresa').click
+    find('div[id="MenuEmpresas"]').click
+    within('table tbody tr') do
+    selecionar = find('td[style="width: 45%;"]')
+    selecionar.hover
+    find('img[class="pointer icons editIcon"]').click
+    end
+
+    # Acessar a empresa direto pela URL
+    # visit '/admin/Empresas'
+    # visit '/admin/Empresas/Create'
+  end
+
+  it 'realizando alteração dos dados da empresa' do
+    expect(find('input[id="Empresa_Codigo"]').readonly?).to be(true)
+    fill_in 'Empresa_CEI', with: '242035439181'
+    fill_in 'Empresa_Telefone', with: '119563245132155'
+    expect(find('input[id="CbUtilizaPortaria"]', visible: false).checked?).to be(true)
+    expect(find('input[id="CbUtilizaPortaria"]', visible: false).disabled?).to be(true)
+    expect(find('input[id="Empresa_RazaoSocial"]').readonly?).to be(true)
+    expect(find('input[id="Empresa_CnpjCpf"]').readonly?).to be(true)
+    drop_ramo = find('#Empresa_RamoAtividade_Id')
+    drop_ramo.all('option').sample.select_option
+    fill_in 'Empresa_Endereco', with: 'rua da madis'
+    fill_in 'Empresa_Bairro', with: 'centro'
+    fill_in 'Empresa_Cidade', with: 'Diadema'
+    fill_in 'Empresa_UF', with: 'SB 12345'
+    fill_in 'Empresa_Pais', with: 'Portugal'
+    fill_in 'Empresa_URL', with: 'https://www.dimepkairos.com.br/Dimep/Empresas/Edit/'
+    fill_in 'Empresa_Email', with: 'dimep@dimep.com'
+    drop_fuso = find('#Empresa_FusoHorario_Id')
+    drop_fuso.all('option').sample.select_option
+    expect(find('input[id="checkHabilitaHorarioVerao"]', visible: false).checked?).to be(true)
+    fill_in 'Empresa_InicioHorarioVerao', with: '10042021'
+    fill_in 'Empresa_FimHorarioVerao', with: '10062021'
+    fill_in 'Empresa_DataPrevisaoProximoFechamento', with: '10062021'
+    fill_in 'Empresa_DataLimiteTratamentoPonto', with: '10082021'
+    fill_in 'connectChave_ChaveConnect', with: 'dsada#@$@#132465'
+    drop = find("input[type=button][value='Salvar']", match: :first)
+    drop.click
+
+    expect(find('div[id=Summary-Field-Index]')).to have_content 'Empresa editada com sucesso'
+   end  
+  end
