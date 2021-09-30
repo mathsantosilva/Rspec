6cff1c59fac5cdd00c43cdd94d638b05ca946a3d
Wagner Xavier <wagner-31@hotmail.com.br>
2021-09-30 17:18:46 -0300

    Validação de resources tela filiais

--- Kairos_Testes_Integrados_BR_qcmaint/spec/cenarios_Integrados/Definicoes/1-Empresa/2-Filiais/4-def_emp_filial_resource_spec.rb
+++ Kairos_Testes_Integrados_BR_qcmaint/spec/cenarios_Integrados/Definicoes/1-Empresa/2-Filiais/4-def_emp_filial_resource_spec.rb
@@ -2,7 +2,7 @@ describe 'criacao filial', :validando_resource_filial do
     before(:each) do
       visit '/'
   
-      fill_in 'LogOnModel_UserName', with: @login
+      fill_in 'LogOnModel_UserName', with: @user_br
       fill_in 'LogOnModel_Password', with: @senha
       click_button 'Entrar'
   
@@ -12,51 +12,78 @@ describe 'criacao filial', :validando_resource_filial do
       sleep 3
       find('div[class="pointer DropDownHeaderElement"]', text: 'Empresa').click
       find('div[id="MenuFiliais"]').click
-      find('label[class="pointer"]').click
   
     end
 
 
-    it 'Validando o Resource no create', :resource_filial do
-        #pagina = page
-        puts 'Validando resources'
-
-        find('div[class="labelFormEdit"]', text:'Código:')
+    it 'Validando os resources', :resource_criacao_filial do
+        #acessar a criação de filiais
+        find('label[class="pointer"]').click
         
-        @elemento = find('div[class="labelFormEdit"]', text:'Código:')
-        @elemento2 = expect(@elemento).to have_content 'Código:'
-
-        puts @elemento2
-
-
-        if @codigo = (expect(@elemento).to have_content 'Código:')
-            
-             puts 'Resource correto'
-            
-        else
-            puts 'Resource incorreto'
-
-        end
-        
-        # find('div[class="labelFormEdit"]', text: 'Telefone:')
-        # find('div[class="labelFormEdit"]', text: 'Razão social:')
-        # pagina.has_field?(find('div[class="labelFormEdit"]', text: 'CNPJ/CPF:'))
-        # pagina.has_field?(find('div[class="labelFormEdit"]', text: 'Endereço:'))
-        # pagina.has_field?(find('div[class="labelFormEdit"]', text: 'Bairro:'))
-        # pagina.has_field?(find('div[class="labelFormEdit"]', text: 'Cidade:'))
-        # pagina.has_field?(find('div[class="labelFormEdit"]', text: 'UF:'))
-        # pagina.has_field?(find('div[class="labelFormEdit"]', text: 'País:'))
-        # pagina.has_field?(find('div[class="labelFormEdit"]', text: 'URL:'))
-        # pagina.has_field?(find('div[class="labelFormEdit"]', text: 'Email:'))
-        # pagina.has_field?(find('div[class="labelFormEdit"]', text: 'Fuso horário:'))
-        # pagina.has_field?(find('div[class="labelFormEdit"]', text: 'Horário de verão:'))
-        # pagina.has_field?(find('div[class="labelFormEdit"]', text: 'Início horário de verão:'))
-        # pagina.has_field?(find('div[class="labelFormEdit"]', text: 'Fim horário de verão:'))
-        # pagina.has_field?(find('div[class="labelFormEdit"]', text: 'Data de Previsão do Próximo Fechamento:'))
-        # pagina.has_field?(find('div[class="labelFormEdit"]', text: 'Data Limite para Tratamento de Ponto:'))
-        # pagina.has_field?(find('div[class="labelFormEdit"]', text: 'Chave do Kairos Connect :'))
-        # pagina.has_field?(find('div[class="labelFormEdit"]', text: 'Localização:'))
-
-    end
+    
+        expect(page).to have_content 'Código:'
+        expect(page).to have_content 'Telefone:'
+        expect(page).to have_content 'Razão social:'
+        expect(page).to have_content 'CNPJ/CPF:'
+        expect(page).to have_content 'Endereço:'
+        expect(page).to have_content 'Bairro:'
+        expect(page).to have_content 'Cidade:'
+        expect(page).to have_content 'UF:'
+        expect(page).to have_content 'País:'
+        expect(page).to have_content 'URL:'
+        expect(page).to have_content 'Email:'
+        expect(page).to have_content 'Fuso horário:'
+        expect(page).to have_content 'Horário de verão:'
+        expect(page).to have_content 'Início horário de verão:'
+        expect(page).to have_content 'Fim horário de verão:'
+        expect(page).to have_content 'Data de Previsão do Próximo Fechamento:'
+        expect(page).to have_content 'Data Limite para Tratamento de Ponto:' 
+        expect(page).to have_content 'Localização:'
+    
+      end 
 
 end
+
+# describe 'Validando Resource em Inglês', :filial_resource_en do
+#     before(:each) do
+#       visit '/'
+  
+#       fill_in 'LogOnModel_UserName', with: @user_en
+#       fill_in 'LogOnModel_Password', with: @senha
+#       click_button 'Entrar'
+      
+#       # Acessando o cadastro da empresa filial pela navegação de telas
+#       selecionar = find('div[id="toggleUserDefinitions"]')
+#       selecionar.hover
+#       sleep 3
+#       find('div[class="pointer DropDownHeaderElement"]', text: 'Empresa').click
+#       find('div[id="MenuFiliais"]').click
+
+#     end
+  
+#     it 'No Create', :resource_criacao_filial_en do
+#       #acessar a criação de filiais
+#       find('label[class="pointer"]').click
+      
+#       expect(page).to have_content 'Code:'
+#       expect(page).to have_content 'telephone:'
+#       expect(page).to have_content 'Company name:'
+#       expect(page).to have_content 'CNPJ/CPF:'
+#       expect(page).to have_content 'Address:'
+#       expect(page).to have_content 'Neighborhood:'
+#       expect(page).to have_content 'City:'
+#       expect(page).to have_content 'UF:'
+#       expect(page).to have_content 'Country:'
+#       expect(page).to have_content 'URL:'
+#       expect(page).to have_content 'E-mail:'
+#       expect(page).to have_content 'DST:'
+#       expect(page).to have_content 'Home DST:'
+#       expect(page).to have_content 'End Daylight Saving Time:'
+#       expect(page).to have_content 'Next Closing Forecast Date:'
+#       expect(page).to have_content 'Deadline for Point Treatment:' 
+#       expect(page).to have_content 'Key of Kairos Connect :'
+#       expect(page).to have_content 'Location:'
+  
+#     end  
+
+# end
