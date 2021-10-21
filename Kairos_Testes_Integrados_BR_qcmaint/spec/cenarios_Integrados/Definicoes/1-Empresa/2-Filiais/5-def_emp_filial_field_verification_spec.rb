4d66f959686ad451d3cce72312866c097f318a68
Wagner Xavier <wagner-31@hotmail.com.br>
2021-10-21 17:33:24 -0300

    Inclusão de uma nova classe para campos obrigatorios e inclusão do novo projeto para campos obrigatorios para filiais

--- Kairos_Testes_Integrados_BR_qcmaint/spec/cenarios_Integrados/Definicoes/1-Empresa/2-Filiais/5-def_emp_filial_field_verification_spec.rb
+++ Kairos_Testes_Integrados_BR_qcmaint/spec/cenarios_Integrados/Definicoes/1-Empresa/2-Filiais/5-def_emp_filial_field_verification_spec.rb
@@ -1,5 +1,37 @@
-#Obrigatoriedade
+require 'common\common'
+require 'common\campos_obrigatorios'
 
-#Valor limite
+common = Common.new
+obrigatorio = Obrigatorios.new
 
-#Tipos caracteres
+describe 'Verificação dos campos', :field_verification_filial do
+
+    describe 'Obrigatoriedades', :field_obrigatoriedade do
+        before(:each) do
+
+            visit '/'
+
+            find('div[class="cc-compliance"]', text: 'Aceitar e fechar').click
+            sleep 1
+
+            # Acessando a empresa filial pela navegação de telas
+            common.logon_br
+
+            # Acessando o cadastro da empresa filial pela navegação de telas
+            common.nav('Empresa','div[id="MenuFiliais"')
+
+        
+        end
+
+        it 'Campos Obrigatorios Filial' do
+
+            obrigatorio.camp_obrig_filiais()
+
+        end
+    end
+
+    #Valor limite
+    
+    #Tipo de caracteres
+    
+end
