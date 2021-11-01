51b23a3900dd624e96afc129a632eacbae482524
Matheus Santos <matheus.santos.x4@gmail.com>
2021-11-01 13:10:00 -0300

    Retirada do cucumber do projeto para mergear para a master

--- Kairos_Testes_Integrados_BR_qcmaint/features/support/env.rb
+++
@@ -1,8 +0,0 @@
-require "allure-cucumber"
-require "appium_lib"
-
-require "fileutils"
-
-# Limpa os logs e os screenshots
-FileUtils.rm_f(Dir.glob("logs/*.json"))
-FileUtils.rm_f(Dir.glob("logs/*.png"))
