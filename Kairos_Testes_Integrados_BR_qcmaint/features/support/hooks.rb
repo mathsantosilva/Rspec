df4338a44804a27703ca66d6e36486b000f8ee81
Matheus Santos <matheus.santos.x4@gmail.com>
2022-03-14 11:29:03 -0300

    Desfazendo a implementação do login

--- Kairos_Testes_Integrados_BR_qcmaint/features/support/hooks.rb
+++ Kairos_Testes_Integrados_BR_qcmaint/features/support/hooks.rb
@@ -6,20 +6,27 @@ require 'cpf_cnpj'
 require 'webdrivers'
 
 Before do
-    # Instanciando as Classes 
-    @dadosusers = Dadosusers.new
-    @common = Common.new
-    @login = Login.new
-    @complements = Complements.new
-    @cadastros = Cadastros.new
-  
-    # solicitar os dados de login no console
-    @login.base()
-    
-  # abrir o navegador maximixado e visitar a pagina solicitada no console 
+
+  $user_br = 'automacao@dimep.com.br'
+  $user_mx = 'teste75624@dimep.com.mx'
+  $user_pt = 'teste75624@dimep.com.pt'
+  $user_en = 'teste75624@dimep.com.en'
+  $senha = '1'
   page.current_window.maximize
-  visit $page
-    
+  @page = 'https://www.dimepkairos.com.br/'
+  @ambiente = 'Dimep'
+  visit @page
+
+
+  # Instanciando as Classes 
+  
+  @login = Login.new
+  @common = Common.new
+  @complements = Complements.new
+  @cadastros = Cadastros.new
+  @obrigatorio = Obrigatorios.new
+
+  
 end
 
   After do |scenario|
