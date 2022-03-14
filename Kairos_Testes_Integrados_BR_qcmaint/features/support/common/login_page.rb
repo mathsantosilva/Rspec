18e321dfa70614225cb461b75f4e9ec11aa99dbb
Matheus Santos <matheus.santos.x4@gmail.com>
2022-03-14 11:13:25 -0300

    rastreando o arquivo login

---
+++ Kairos_Testes_Integrados_BR_qcmaint/features/support/common/login_page.rb
@@ -0,0 +1,41 @@
+class Login
+
+    include Capybara::DSL
+    include RSpec::Matchers 
+
+    def logar(user, password)
+        fill_in 'LogOnModel_UserName', with: user
+        fill_in 'LogOnModel_Password', with: password
+        find('button[id="btnFormLogin"]').click
+    end
+
+
+    #Realizando o login BR
+    def logon_br()
+        fill_in 'LogOnModel_UserName', with: $user_ptbr
+        fill_in 'LogOnModel_Password', with: $senha
+        find('button[id="btnFormLogin"]').click 
+        # find('input[id="btnFormLogin"]').click
+    end
+
+    #Realizando o login mx
+    def logon_mx()
+        fill_in 'LogOnModel_UserName', with: $user_es
+        fill_in 'LogOnModel_Password', with: $senha
+        find('button[id="btnFormLogin"]').click
+    end
+
+    #Realizando o login pt
+    def logon_pt()
+        fill_in 'LogOnModel_UserName', with: $user_ptpt
+        fill_in 'LogOnModel_Password', with: $senha
+        find('button[id="btnFormLogin"]').click
+    end
+
+    #Realizando o login en
+    def logon_en()
+        fill_in 'LogOnModel_UserName', with: $user_en
+        fill_in 'LogOnModel_Password', with: $senha
+        find('button[id="btnFormLogin"]').click
+    end
+end
